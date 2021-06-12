47fca7614fdb44d52512902f4af570bef92c8596
aga7hokakological <iamsaurabhmandavkar@gmail.com>
2021-06-12 23:49:04 +0530

    Changes to file

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,12 @@
 use reqwest::Url;
 
-use std::io::{self, BufRead};
-use std::fs::File;
-use std::path::Path;
-use std::result::Result as OtherResult;
-use clap::{App, Arg};
-use std::ffi::OsString;
-
 use error_chain::error_chain;
 
-#[cfg(test)]
-extern crate quickcheck;
-#[cfg(test)]
-#[macro_use(quickcheck)]
-extern crate quickcheck_macros;
-
-#[derive(Debug, PartialEq)]
-struct Fuzz {
-    url: String,
-    file: String,
-}
-
-impl Fuzz {
-    fn new() -> Self {
-        Self::new_from(std::env::args_os().into_iter()).unwrap_or_else(|e| e.exit())
-    }
-
-    fn new_from<I, T>(_args: I) -> OtherResult<Self, clap::Error>
-    where
-        I: Iterator<Item = T>,
-        T: Into<OsString> + Clone,
-    {
-        // basic app information
-        let app = App::new("fuzzWeb")
-            .version("1.0")
-            .about("Does awesome things")
-            .author("Saurabh Mandavkar")
-            .arg(Arg::with_name("url")
-                .long("url") // allow --name
-                .short("u") // allow -n
-                .takes_value(true)
-                .help("Enter url to fuzz")
-                .required(true))
-            .arg(Arg::with_name("file")
-                .long("file") // allow --name
-                .short("f") // allow -n
-                .takes_value(true)
-                .help("Enter fuzzz file")
-                .required(true))
-            .get_matches();
-
-        // Extract the actual name
-        let url1 = app
-            .value_of("url")
-            .expect("This can't be None, we said it was required");
-
-        let file1 = app
-            .value_of("file")
-            .expect("This can't be None, we said it was required");
-
-        Ok(Fuzz {
-            url: url1.to_string(),
-            file: file1.to_string(),
-        })
-    }
-}
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
+use std::thread;
+use std::io::{self, prelude::*};
+use std::path::Path;
+use std::fs::File;
+use clap::{App, Arg};
 
 error_chain! {
     foreign_links {
@@ -79,74 +15,61 @@ error_chain! {
     }
 }
 
-async fn fuzzer(url: Url) -> Result<()> {
-    let res = reqwest::get(url).await?;
-    println!("Status: {}", res.status());
+#[tokio::main]
+async fn fuzz(target: Url) -> Result<()> {
+    let res = reqwest::get(target).await?;
+
+    println!("{:?}", res.status());
 
     Ok(())
 }
 
-#[tokio::main(flavor = "current_thread")]
+fn read_file<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path> {
+        let file = File::open(filename)?;
+        Ok(io::BufReader::new(file).lines())
+    }
+#[tokio::main]
 async fn main() -> Result<()> {
-    let fuzzweb = Fuzz::new();
 
-    let url = Url::parse(&fuzzweb.url).unwrap();
+    let app = App::new("fuzzWeb")
+            .version("1.0")
+            .about("Does awesome things")
+            .author("Saurabh Mandavkar")
+            .args_from_usage(
+                "
+                -u, --url=[TARGET_URL] 'set your target URL(required)'
+                -w, --wordlist=[PATH_TO_WORDLIST] 'set your wordlist(required)'
+                -t, --timeout=[SECONDS] 'set the timeout time in seconds Default(15)'
+                "
+            )
+            .arg(
+                Arg::with_name("v")
+                .short("v")
+                .long("verbose")
+                .help("show verbose output")
+            )
+            .get_matches();
 
-    if let Ok(lines) = read_lines(&fuzzweb.file) {
-        // Consumes the iterator, returns an (Optional) String
+    // Extract the actual name
+    let target = app.value_of("url").unwrap();
+    let wordlist = app.value_of("wordlist").unwrap();
+    let mut _timeout = 15;
+
+    let url = Url::parse(&target).unwrap();
+
+    if let Ok(lines) = read_file(&wordlist) {
         for line in lines {
             if let Ok(fuzzword) = line {
                 let url_to_fuzz = url.join(&fuzzword);
-                let url1 = url_to_fuzz.unwrap();
-                fuzzer(url1).await?;
+                let target_url = url_to_fuzz.unwrap();
+
+                thread::spawn(|| {
+                    fuzz(target_url);
+                }).join().expect("Thread panicked")
             }
         }
     }
+
     Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_no_args() {
-        Fuzz::new_from(["exename"].iter()).unwrap_err();
-    }
-
-    #[test]
-    fn test_incomplete_name() {
-        Fuzz::new_from(["exename", "--name"].iter()).unwrap_err();
-    }
-
-    #[test]
-    fn test_complete_name() {
-        assert_eq!(
-            Fuzz::new_from(["exename", "--name", "Hello"].iter()).unwrap(),
-            Fuzz { url: "Hello".to_string(), file: "Yo".to_string() }
-        );
-    }
-
-    #[test]
-    fn test_short_name() {
-        assert_eq!(
-            Fuzz::new_from(["exename", "-n", "Hello"].iter()).unwrap(),
-            Fuzz { name: "Hello".to_string(), file: "Yo".to_string()  }
-        );
-    }
-
-    /* This property will fail, can you guess why?
-    #[quickcheck]
-    fn prop_any_name(name: String) {
-        assert_eq!(
-            HelloArgs::new_from(["exename", "-n", &name].iter()).unwrap(),
-            HelloArgs { name }
-        );
-    }
-    */
-
-    #[quickcheck]
-    fn prop_never_panics(args: Vec<String>) {
-        let _ignored = Fuzz::new_from(args.iter());
-    }
 }
