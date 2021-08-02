172d9bc1165b5d412291aa2d7d1637e346a50594
aga7hokakological <iamsaurabhmandavkar@gmail.com>
2021-08-02 10:31:42 +0530

    request timeout added

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use reqwest::{Url, Client};
 use futures::future;
 use clap::{App, Arg};
+// use hyper::Client;
 use tokio::time::Duration;
 
 use error_chain::error_chain;
@@ -9,6 +10,7 @@ use std::thread;
 use std::vec::Vec;
 use std::io::{self, prelude::*};
 use std::path::Path;
+// use std::ffi::OsStr;
 use std::fs::File;
 
 error_chain! {
@@ -19,8 +21,8 @@ error_chain! {
 }
 
 #[tokio::main]
-async fn fuzz(urls: Vec<Url>) -> Result<()> {
-    let timeout_duration = 10;
+async fn fuzz(urls: Vec<Url>, mut path: File, timeout: u64) -> Result<()> {
+    // let timeout_duration = 10;
     let client = Client::new();
 
     // let res = reqwest::get(target).await?;
@@ -28,7 +30,7 @@ async fn fuzz(urls: Vec<Url>) -> Result<()> {
     let res = future::join_all(urls.into_iter().map(|url| {
         let client = &client;
         async move {
-            let resp = client.get(url).timeout(Duration::from_secs(timeout_duration)).send().await;
+            let resp = client.get(url).timeout(Duration::from_secs(timeout)).send().await;
             // println!("{:?}", resp);
             resp.unwrap()
         }
@@ -44,6 +46,7 @@ async fn fuzz(urls: Vec<Url>) -> Result<()> {
         // }
 
         println!("{:?}: {:?}", r.url().as_str(), r.status());
+        write!(path, "{:?}: {:?}\n", r.url().as_str(), r.status());
     }
 
     Ok(())
@@ -67,6 +70,7 @@ async fn main() -> Result<()> {
                 -u, --url=[TARGET_URL] 'set your target URL(required)'
                 -w, --wordlist=[PATH_TO_WORDLIST] 'set your wordlist(required)'
                 -t, --timeout=[SECONDS] 'set the timeout time in seconds Default(15)'
+                -o, --output=[PATH_TO_SAVE_FILE] 'Enter path to save file'
                 "
             )
             .arg(
@@ -80,10 +84,14 @@ async fn main() -> Result<()> {
     // Extract the actual name
     let target = app.value_of("url").unwrap();
     let wordlist = app.value_of("wordlist").unwrap();
+    let output = app.value_of("output").unwrap();
+    let timeout = app.value_of("timeout").unwrap();
+    let t = timeout.parse::<u64>().unwrap();
     
     let mut new_vec = Vec::new();
+    let out_path = File::create(output)?;
     
-    let url = Url::parse(&target).unwrap();
+    let url = Url::parse(target).unwrap();
 
     if let Ok(lines) = read_file(&wordlist) {
         for line in lines {
@@ -96,8 +104,9 @@ async fn main() -> Result<()> {
         }
     }
 
-    thread::spawn(|| {
-        fuzz(new_vec).expect("Execution or files not handled properly");
+    thread::spawn(move || {
+        fuzz(new_vec, out_path, t).expect("Execution or files not handled properly
+        ");
     }).join().expect("Thread panicked");
 
     Ok(())
